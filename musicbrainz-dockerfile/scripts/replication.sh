94e79350f68206b9a9c30f82bd689ea48b264fe5
ta264 <ta264@users.noreply.github.com>
2019-05-06 18:48:14 +0100

    Only run the replication job on loop

--- musicbrainz-dockerfile/scripts/replication.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-eval $( /usr/bin/perl -Mlocal::lib )
-
-/bin/bash /musicbrainz-server/admin/cron/slave.sh
--- musicbrainz-dockerfile/scripts/start.sh
+++ musicbrainz-dockerfile/scripts/start.sh
@@ -2,6 +2,9 @@
 
 eval $( perl -Mlocal::lib )
 
-cron -f &
-/start_mb_renderer.pl
-start_server --port=5000 -- plackup -I lib -s Starlet -E deployment --nproc 10 --pid fcgi.pid
+while true
+do
+    # Sleep first to make sure everything is up before starting replication
+    sleep 5m
+    /musicbrainz-server/admin/replication/LoadReplicationChanges
+done
